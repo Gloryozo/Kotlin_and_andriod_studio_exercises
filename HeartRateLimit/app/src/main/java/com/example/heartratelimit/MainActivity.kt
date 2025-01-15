de9a373cc7c2a8a5c92522c548700a7a14dbf90d
Gloryozo <gloryozoji@gmail.com>
2025-01-15 12:10:20 +0200

    Added more upper and lower limit to calculate heart rate limit.

--- HeartRateLimit/app/src/main/java/com/example/heartratelimit/MainActivity.kt
+++ HeartRateLimit/app/src/main/java/com/example/heartratelimit/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.heartratelimit
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.heartratelimit.ui.theme.HeartRateLimitTheme
+import java.math.RoundingMode
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HeartRateLimitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HeartRateLimit(
+                    HeartRateLimits(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,17 +44,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HeartRateLimit(modifier: Modifier = Modifier) {
-    Text(
-        text = "Your heart rate limits are",
-        modifier = modifier
-    )
+fun HeartRateLimits(modifier: Modifier = Modifier) {
+    var ageInput by remember { mutableStateOf("") } // State variable for age
+    val age = ageInput.toIntOrNull() ?: 0 // Age as int
+    val upper = if (age > 0) (220 - age) * 0.85f else 0// Upper limit
+    val lower = if (age > 0) (220 - age) * 0.65f else 0// Lower limit
+    val df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.CEILING
+    Column (
+        modifier = modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+    ){
+        TextField(
+            value = ageInput,
+            onValueChange = {ageInput = it},
+            label = {Text(text = "Age")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(
+            text =
+                stringResource(R.string.heart_rate_limits,df.format(lower),df.format(upper)),
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HeartRateLimitsPreview() {
     HeartRateLimitTheme {
-        HeartRateLimit()
+        HeartRateLimits()
     }
 }
